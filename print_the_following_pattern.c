095539ae6f365e49abe91f315d8a1c2c5052c157
vijayasri14 <119839921+vijayasri14@users.noreply.github.com>
2023-05-03 11:05:00 +0530

    #include<stdio.h> int main() {     int n,i,j,x;     scanf("%d",&n);     x=65+n-1;     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",x);         }         printf(" ");         x=x-1;     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,36 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,k;
+    int n,i,j,x;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    x=65+n-1;
+    for(i=n;i>=1;i--)
     {
-        for(j=1;j<=n-i;j++)
+        for(j=1;j<=i;j++)
         {
-            printf(" ");
-        }
-        for(k=1;k<=n;k++)
-        {
-            if(i==1 || i==n || k==1|| k==n)
-            {
-                printf("*");
-            }
-            else
-            {
-                printf(" ");
-            }
+            printf("%c ",x);
         }
         printf("
 ");
+        x=x-1;
     }
+    
 }
-/*n=int(input())
-for i in range(1,n+1):
-    for j in range(n,i,-1):
-        print(" ",end="")
-    for l in range(0,n):
-        if(i==1 or i==n or l==0 or l==n-1):
-            print("*",end="")
-        else:
-            print(" ",end="")
-    print()*/
